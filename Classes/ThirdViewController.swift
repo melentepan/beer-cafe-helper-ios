69ca134647c9ca87f3a94a96675d9968d18d41f9
melentepan <81905434+melentepan@users.noreply.github.com>
2023-04-10 19:02:15 +0300

    New ViewController

--- Classes/ThirdViewController.swift
+++ Classes/ThirdViewController.swift
@@ -1,8 +1,5 @@
-//
-//  ThirdViewController.swift
-//  Beer cafe helper
-//
-//  Created by Мелентьев Степан on 10.04.2023.
-//
+import UIKit
 
-import Foundation
+class ThirdViewController: UIViewController {
+    
+}
