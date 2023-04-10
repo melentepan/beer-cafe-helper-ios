a1157c6080e3b58541576054100b914befd7efe4
melentepan <81905434+melentepan@users.noreply.github.com>
2023-04-10 17:16:09 +0300

    New classes and new button

---
+++ Classes/Beer.swift
@@ -0,0 +1,8 @@
+//
+//  Beer.swift
+//  Beer cafe helper
+//
+//  Created by Мелентьев Степан on 10.04.2023.
+//
+
+import Foundation
---
+++ Classes/Manager.swift
@@ -0,0 +1,8 @@
+//
+//  Manager.swift
+//  Beer cafe helper
+//
+//  Created by Мелентьев Степан on 10.04.2023.
+//
+
+import Foundation
