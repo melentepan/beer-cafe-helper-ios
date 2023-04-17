fa6a17b38006194b45294c05bd5358ae2f9751b1
melentepan <81905434+melentepan@users.noreply.github.com>
2023-04-18 00:04:33 +0300

    Create ThirdVC

--- Beer cafe helper/Classes/ThirdViewController.swift
+++ Beer cafe helper/Classes/ThirdViewController.swift
@@ -2,4 +2,23 @@ import UIKit
 
 class ThirdViewController: UIViewController {
     
+    @IBOutlet weak var beerNameTextField: UITextField!
+    @IBOutlet weak var countPerDayTextField: UITextField!
+    @IBOutlet weak var PriceTextField: UITextField!
+    
+    @IBOutlet weak var AddBeerClassButton: UIButton!
+    
+    
+    @IBAction func AddBeerClassButtonPressed(_ sender: UIButton) {
+        guard let name = beerNameTextField.text else {return}
+        guard let count = Int(countPerDayTextField.text!) else {return}
+        guard let price = Float(PriceTextField.text!) else {return}
+        Manager.shared.beerArray.append(Beer(name: name, price: price, countPerDay: count))
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
 }
